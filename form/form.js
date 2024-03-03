ae6694f9548367e6058de446c660230736324c0e
SumayiaOishi <sumayia.alam04@gmail.com>
2024-03-03 21:22:36 +0600

    Making new login page

--- form/form.js
+++ form/form.js
@@ -1,41 +1,31 @@
-
-// let button = document.querySelector('.js-button').addEventListener('click',function(){
-//  let input =document.querySelector('.js-input').value;
-
-//  if(input=== ''){
-//    document.getElementById('demo').innerHTML="Fill up ";
-//  }
-
-// });
-
-// form.js
 let button = document.querySelector('.js-button').addEventListener('click', function () {
   let input = document.querySelectorAll('.js-input');
   let para = document.querySelectorAll('.demo');
   let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$/;
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   for (let i = 0; i < input.length; i++) {
-    if (input[i].value === '') {
-      para[i].innerHTML = "Please fill up this field";
-    } else {
-      para[i].innerHTML = ""; 
-    }
+      if (input[i].value === '') {
+          para[i].textContent = "Please fill up this field";
+      } else {
+          para[i].textContent = "";
+      }
+  }
+
+  let email = document.querySelector('input[type="email"]').value;
+  if (!emailRegex.test(email)) {
+      para[3].textContent = "Please enter a valid email address";
+  } else {
+      para[3].textContent = "";
   }
 
   let password = document.querySelector('input[type="password"]').value;
   if (!passwordRegex.test(password)) {
-    para[input.length - 1].innerHTML = "Password must be at least 8 characters";
+      para[4].textContent = "Password must be at least 8 characters";
   } else {
-    para[input.length - 1].innerHTML = ""; 
+      para[4].textContent = "";
   }
-
-  let formData='';
-  input.forEach(inputs=>{
-    formData += `<P>${inputs.placeholder}:${inputs.value}</P>`
-  });
-  document.getElementById('form-data').innerHTML=formData;
-  
-  
 });
+
 
 
